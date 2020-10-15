b7de848dea2699ecfd68e29d36b0401a2d7de775
Breno <breno2019222@gmail.com>
2020-10-15 19:15:29 -0300

    Services and Phone Updates

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
 let menuBtn = document.querySelector("nav > a");
 let menuContent = document.querySelector(".menu");
 let menuBtnStatus = false;
+const y = window.matchMedia("(max-width: 1024px)");
+
+function menuFunction() {
+  if (y.matches) {
+    menuContent.style.display = flex;
+  }
+}
 
 function toggleMenu(e) {
   e.preventDefault();
